b276c5f5a32b09c03511bb063de672066ad6d188
dilanm1 <dilanm1@yahoo.co.uk>
2015-01-28 16:28:12 +0000

    ui

--- CarDealer/Site.Master.designer.cs
+++ CarDealer/Site.Master.designer.cs
@@ -20,5 +20,14 @@ namespace CarDealer {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
+        
+        /// <summary>
+        /// news_go control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button news_go;
     }
 }
